b12ee093b3820d00e30870bd7c47eca9d3df1bc7
kiran <kiran03.k2gmail.com>
2025-09-15 14:14:40 +0000

    updated-by-kiran

--- Msg.java
+++ Msg.java
@@ -1,5 +1,7 @@
 public class Msg{
 	public static void main(String[] args){
 		System.out.println("This is msg-app done by krishna");
+		System.out.println("New features added by kiran");
+		System.out.println("WELCOME TO JAVA-APP");
 	}
 }
